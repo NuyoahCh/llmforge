bfcd7477c7fcdb0c8224231fe984ce920a885a21
Morning <springoflotus@gmail.com>
2025-12-15 16:55:24 +0800

    opt：更新优化内容

--- 06Project/Medical/zoe-ChatLLM-medical-main-zoe/src/main/java/com/zoe/java/ai/langchain4j/service/AppointmentService.java
+++
@@ -1,9 +0,0 @@
-package com.zoe.java.ai.langchain4j.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.zoe.java.ai.langchain4j.entity.Appointment;
-
-public interface AppointmentService extends IService<Appointment> {
-    //IService中有一些预定义的方法可以直接使用
-    Appointment getOne(Appointment appointment);
-}
